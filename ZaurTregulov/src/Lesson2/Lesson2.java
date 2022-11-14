291ba268564c63ff9eb0c7a66faee0cf7dc396fc
Cinnamonbun1233 <cinnamonbun1233@yandex.ru>
2022-11-14 19:14:29 +0300

    Lesson2 and Homework2 done

---
+++ ZaurTregulov/src/Lesson2/Lesson2.java
@@ -0,0 +1,48 @@
+package Lesson2;
+
+public class Lesson2 {
+
+    public static void main(String[] args) {
+
+        byte b1 = 10;
+        byte b2 = 20;
+        byte b3 = 100;
+
+        short s1 = 5;
+        short s2 = -10;
+        short s3 = 0;
+
+        int i1 = 500;
+
+        long l1 = 1000;
+        long l2 = 10_000_000_000L;
+        long l3 = 5L;
+
+        float f1 = 3.14F;
+        float f2 = 2.5F;
+        float f3 = 20;
+
+        double d1 = 5.5;
+        double d2 = 87.65;
+
+        char c1 = 'a';
+        char c2 = 'A';
+        char c3 = '7';
+        char c4 = ' ';
+        char c5 = 300; //10-ричная система счисления
+        char c6 = '\u1234'; //16-ричная система счисления
+
+        boolean bool1 = true;
+        boolean bool2 = false;
+
+        int a1 = 60; //10-ричная система счисления
+        int a2 = 0B111100; //2-ричная система счисления
+        int a3 = 074; //8-ричная система счисления
+        int a4 = 0x3C; //16-ричная система счисления
+
+        System.out.println(a1);
+        System.out.println(a2);
+        System.out.println(a3);
+        System.out.println(a4);
+    }
+}
