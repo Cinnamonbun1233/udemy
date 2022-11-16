c4b15a7c51d1b4370d69c210f887cf7a3527023a
Cinnamonbun1233 <cinnamonbun1233@yandex.ru>
2022-11-16 12:34:33 +0300

    Lesson8 and Homework8 done

---
+++ ZaurTregulov/src/Homework8/Math.java
@@ -0,0 +1,21 @@
+package Homework8;
+
+public class Math {
+
+    public static int multiplicationThreeNumbers(int a, int b, int c) {
+        return a * b * c;
+    }
+
+    public static void dividingTwoNumbers(int a, int b) {
+        System.out.println("Результат деления " + a + " на " + b + ": целое частное - " + a / b + " и остаток - " + a % b);
+    }
+}
+
+class MathTest {
+
+    public static void main(String[] args) {
+
+        System.out.println(Math.multiplicationThreeNumbers(1, 2, 3));
+        Math.dividingTwoNumbers(14, 6);
+    }
+}
