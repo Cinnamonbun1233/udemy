2234e4106ad277eca56f734fc720f332355434c4
Cinnamonbun1233 <cinnamonbun1233@yandex.ru>
2022-11-24 20:36:19 +0300

    Lesson19 and Homework19 done

---
+++ ZaurTregulov/src/Lessons/Lesson_19/Test_1.java
@@ -0,0 +1,22 @@
+package Lessons.Lesson_19;
+
+public class Test_1 {
+
+    static void sum(int... a) { //принимает любое кол-во параметров
+        int sum = 0;
+        for (int j : a) {
+            sum += j;
+        }
+        System.out.println(sum);
+    }
+
+    public static void main(String[] args) {
+
+        sum();
+        sum(1);
+        sum(1, 2);
+        sum(1, 2, 3);
+        sum(1, 2, 3, 4);
+        sum(1, 2, 3, 4, 5);
+    }
+}
---
+++ ZaurTregulov/src/Lessons/Lesson_19/Test_6.java
@@ -0,0 +1,31 @@
+package Lessons.Lesson_19;
+
+import java.util.Arrays;
+
+public class Test_6 {
+
+    public static void main(String[] args) {
+
+        int[] array = {0, 6, 4, 1};
+        Arrays.fill(array, 3);
+
+        StringBuilder[] array2 = {new StringBuilder("hello"), new StringBuilder("good buy"), new StringBuilder("ok")};
+        for (StringBuilder s : array2) {
+            s.replace(0,s.length(),"Hi-hi");
+        }
+
+        for (int a : array) {
+            System.out.print(a + "  ");
+        }
+
+        System.out.println();
+
+        for (StringBuilder s : array2) {
+            System.out.print(s + " ");
+        }
+
+        System.out.println();
+
+        System.out.println(array2[0]);
+    }
+}
