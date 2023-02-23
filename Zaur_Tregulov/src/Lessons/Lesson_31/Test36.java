567cd7a71568774f8cf8aa26989da3d11d9b28a6
dima <cinnamonbun1233@outlook.com>
2023-02-23 14:18:06 +0300

    Update

---
+++ Zaur_Tregulov/src/Lessons/Lesson_31/Test36.java
@@ -0,0 +1,17 @@
+package Lessons.Lesson_31;
+
+public class Test36 {
+}
+
+//class Test {
+//    public static void main(String[] args) {
+//        String s1 = "ok";
+//        String s2 = s1;
+//        s1 += "!!!";
+//        System.out.println(s1 + ", " + s2 + ", " + (s1 == s2) + ", " + s1.equals(s2));
+//        StringBuilder sb1 = new StringBuilder("ok");
+//        StringBuilder sb2 = sb1;
+//        sb1.append("!!!");
+//        System.out.println(sb1 + ", " + sb2 + ", " + (sb1 == sb2) + ", " + sb1.equals(sb2));
+//    }
+//}
---
+++ Zaur_Tregulov/src/Lessons/Lesson_31/Test37.java
@@ -0,0 +1,13 @@
+package Lessons.Lesson_31;
+
+public class Test37 {
+}
+
+//class Test {
+//    public static void main(String[] args) {
+//        while (true) {
+//            System.out.println("privet");
+//        }
+//        System.out.println("poka");
+//    }
+//}
