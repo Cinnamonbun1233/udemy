161d150a4162d4735c39e35959633aee6466a324
cinnamonbun1233 <cinnamonbun1233@yandex.ru>
2023-02-24 14:47:06 +0300

    Update

---
+++ Zaur_Tregulov/src/JavaGetTheBlackBelt/Lessons/Lesson_13/ArrayLIstMethods4.java
@@ -0,0 +1,19 @@
+package JavaGetTheBlackBelt.Lessons.Lesson_13;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class ArrayLIstMethods4 {
+    public static void main(String[] args) {
+        StringBuilder sb1 = new StringBuilder("A");
+        StringBuilder sb2 = new StringBuilder("B");
+        StringBuilder sb3 = new StringBuilder("C");
+        StringBuilder sb4 = new StringBuilder("D");
+        StringBuilder[] array = {sb1, sb2, sb3, sb4};
+        List<StringBuilder> list = Arrays.asList(array);
+        System.out.println(list);
+//        array[0].append("!!!");
+        array[0] = new StringBuilder("F");
+        System.out.println(list);
+    }
+}
