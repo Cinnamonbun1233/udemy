9b183b9708d284ee70b088dd20db7cdbb5afae74
cinnamonbun1233 <cinnamonbun1233@yandex.ru>
2023-02-24 19:15:18 +0300

    Update

---
+++ Zaur_Tregulov/src/JavaGetTheBlackBelt/Lesson_21/HashCodeEx1.java
@@ -0,0 +1,70 @@
+package JavaGetTheBlackBelt.Lesson_21;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+
+public class HashCodeEx1 {
+    public static void main(String[] args) {
+        Map<Student, Double> map = new HashMap<>();
+        Student st1 = new Student("Заур", "Трегулов", 3);
+        Student st2 = new Student("Мария", "Иванова", 1);
+        Student st3 = new Student("Сергей", "Петров", 4);
+        map.put(st1, 7.5);
+        map.put(st2, 8.7);
+        map.put(st3, 9.2);
+        System.out.println(map);
+
+        Student st4 = new Student("Заур", "Трегулов", 3);
+        Student st5 = new Student("Игорь", "Сидоров", 4);
+
+//        boolean result = map.containsKey(st4);
+//        System.out.println("result = " + result);
+//        System.out.println(st1.equals(st4));
+
+        System.out.println(st1.hashCode());
+        System.out.println(st2.hashCode());
+        System.out.println(st3.hashCode());
+        System.out.println(st4.hashCode());
+        System.out.println(st5.hashCode());
+    }
+}
+
+class Student {
+    String name;
+    String surname;
+    int course;
+
+    public Student(String name, String surname, int course) {
+        this.name = name;
+        this.surname = surname;
+        this.course = course;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "name='" + name + '\'' +
+                ", surname='" + surname + '\'' +
+                ", course=" + course +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Student student = (Student) o;
+        return course == student.course && Objects.equals(name, student.name) && Objects.equals(surname, student.surname);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, surname, course);
+    }
+
+//    @Override
+//    public int hashCode() {
+//        return name.length() * 7 + surname.length() * 11 + course * 53;
+//    }
+}
