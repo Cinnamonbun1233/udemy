24ce2cef3d5fc9366fbc0a3e258e64f49c4c21c3
cinnamonbun1233 <cinnamonbun1233@yandex.ru>
2023-02-25 18:05:48 +0300

    Update

---
+++ Zaur_Tregulov/src/JavaGetTheBlackBelt/Lesson_32/Test.java
@@ -0,0 +1,16 @@
+package JavaGetTheBlackBelt.Lesson_32;
+
+public class Test {
+    void method() {
+        class C {
+
+        }
+    }
+
+    static class A {
+    }
+
+    class B {
+
+    }
+}
