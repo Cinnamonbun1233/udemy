926a346a5a1c04fbb49f58a1a500b63c2b291cff
cinnamonbun1233 <cinnamonbun1233@yandex.ru>
2023-02-25 17:52:04 +0300

    Update

---
+++ Zaur_Tregulov/src/JavaGetTheBlackBelt/Lesson_30/PriorityQueueEx1.java
@@ -0,0 +1,20 @@
+package JavaGetTheBlackBelt.Lesson_30;
+
+import java.util.PriorityQueue;
+
+public class PriorityQueueEx1 {
+    public static void main(String[] args) {
+        PriorityQueue<Integer> priorityQueue = new PriorityQueue<>();
+        priorityQueue.add(4);
+        priorityQueue.add(1);
+        priorityQueue.add(7);
+        priorityQueue.add(10);
+        priorityQueue.add(8);
+        System.out.println(priorityQueue.peek());
+        System.out.println(priorityQueue.remove());
+        System.out.println(priorityQueue.remove());
+        System.out.println(priorityQueue.remove());
+        System.out.println(priorityQueue.remove());
+        System.out.println(priorityQueue.remove());
+    }
+}
---
+++ Zaur_Tregulov/src/JavaGetTheBlackBelt/Lesson_30/PriorityQueueEx2.java
@@ -0,0 +1,66 @@
+package JavaGetTheBlackBelt.Lesson_30;
+
+import java.util.Objects;
+import java.util.PriorityQueue;
+
+public class PriorityQueueEx2 {
+    public static void main(String[] args) {
+        Student st1 = new Student("Заур", 3);
+        Student st2 = new Student("Мария", 1);
+        Student st3 = new Student("Сергей", 4);
+        Student st4 = new Student("Игорь", 2);
+        Student st5 = new Student("Василий", 1);
+        Student st6 = new Student("Олег", 3);
+        PriorityQueue<Student> priorityQueue = new PriorityQueue<>();
+        priorityQueue.add(st1);
+        priorityQueue.add(st2);
+        priorityQueue.add(st3);
+        priorityQueue.add(st4);
+        priorityQueue.add(st5);
+        priorityQueue.add(st6);
+        System.out.println(priorityQueue);
+        System.out.println(priorityQueue.poll());
+        System.out.println(priorityQueue.poll());
+        System.out.println(priorityQueue.poll());
+        System.out.println(priorityQueue.poll());
+        System.out.println(priorityQueue.poll());
+        System.out.println(priorityQueue.poll());
+        System.out.println(priorityQueue);
+    }
+}
+
+class Student implements Comparable<Student> {
+    String name;
+    int course;
+
+    public Student(String name, int course) {
+        this.name = name;
+        this.course = course;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Student student = (Student) o;
+        return course == student.course && Objects.equals(name, student.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, course);
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "name='" + name + '\'' +
+                ", course=" + course +
+                '}';
+    }
+
+    @Override
+    public int compareTo(Student student) {
+        return this.course - student.course;
+    }
+}
