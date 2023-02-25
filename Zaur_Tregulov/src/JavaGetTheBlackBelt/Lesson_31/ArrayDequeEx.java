96c70cbf2055557fb042d52cc6ad35570ee663a1
cinnamonbun1233 <cinnamonbun1233@yandex.ru>
2023-02-25 18:01:02 +0300

    Update

---
+++ Zaur_Tregulov/src/JavaGetTheBlackBelt/Lesson_31/ArrayDequeEx.java
@@ -0,0 +1,28 @@
+package JavaGetTheBlackBelt.Lesson_31;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+public class ArrayDequeEx {
+    public static void main(String[] args) {
+        Deque<Integer> deque = new ArrayDeque<>();
+        deque.addFirst(3);
+        deque.addFirst(5);
+        deque.addLast(7);
+        deque.offerFirst(1);
+        deque.offerLast(8);
+        System.out.println(deque);
+        System.out.println(deque.getFirst());
+        System.out.println(deque.getLast());
+        System.out.println(deque.removeFirst());
+        System.out.println(deque);
+        System.out.println(deque.peekFirst());
+        System.out.println(deque.peekLast());
+        System.out.println(deque.removeLast());
+        System.out.println(deque);
+        System.out.println(deque.pollFirst());
+        System.out.println(deque);
+        System.out.println(deque.pollLast());
+        System.out.println(deque);
+    }
+}
