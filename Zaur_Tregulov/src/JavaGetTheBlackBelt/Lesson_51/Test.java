b37422b59ce5d5cb43bd77729f2b4c2c789a6f3b
cinnamonbun1233 <cinnamonbun1233@yandex.ru>
2023-02-26 20:40:52 +0300

    Update

--- Zaur_Tregulov/src/JavaGetTheBlackBelt/Lesson_51/Test.java
+++ Zaur_Tregulov/src/JavaGetTheBlackBelt/Lesson_51/Test.java
@@ -1,10 +1,11 @@
-package JavaGetTheBlackBelt.Lesson_50;
+package JavaGetTheBlackBelt.Lesson_51;
 
 import java.util.stream.Stream;
 
 public class Test {
     public static void main(String[] args) {
         Stream<Integer> stream1 = Stream.of(1, 2, 3, 4, 5, 1, 2, 3);
-        stream1.distinct().forEach(System.out::println);
+//        System.out.println(stream1.count());
+        System.out.println(stream1.distinct().count());
     }
 }
