c8093465323c0e8747a4a5ce59001908bbff012a
cinnamonbun1233 <cinnamonbun1233@yandex.ru>
2023-02-26 10:00:26 +0300

    Update

--- Zaur_Tregulov/src/JavaGetTheBlackBelt/Lesson_37/Test2.java
+++ Zaur_Tregulov/src/JavaGetTheBlackBelt/Lesson_37/Test2.java
@@ -2,14 +2,17 @@ package JavaGetTheBlackBelt.Lesson_37;
 
 interface I {
     int abc(String s);
+//    int abc();
 }
 
 public class Test2 {
     static void def(I i) {
-        System.out.println(i.abc("String"));
+        System.out.println(i.abc("Privet"));
     }
 
     public static void main(String[] args) {
+//        def(() -> 18);
+        int i = 10;
         def(String::length);
     }
 }
---
+++ Zaur_Tregulov/src/JavaGetTheBlackBelt/Lesson_38/Student.java
@@ -0,0 +1,69 @@
+package JavaGetTheBlackBelt.Lesson_38;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+
+interface StudentChecks {
+    boolean check(Student student);
+}
+
+public class Student {
+    String name;
+    char sex;
+    int age;
+    int course;
+    double avgGrade;
+
+    public Student(String name, char sex, int age, int course, double avgGrade) {
+        this.name = name;
+        this.sex = sex;
+        this.age = age;
+        this.course = course;
+        this.avgGrade = avgGrade;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "name='" + name + '\'' +
+                ", sex=" + sex +
+                ", age=" + age +
+                ", course=" + course +
+                ", avgGrade=" + avgGrade +
+                '}';
+    }
+}
+
+class StudentInfo {
+    public static void main(String[] args) {
+        Student st1 = new Student("Иван", 'м', 22, 3, 8.3);
+        Student st2 = new Student("Николай", 'м', 28, 2, 6.4);
+        Student st3 = new Student("Елена", 'ж', 19, 1, 8.9);
+        Student st4 = new Student("Пётр", 'м', 35, 4, 7.0);
+        Student st5 = new Student("Мария", 'ж', 23, 3, 9.1);
+        ArrayList<Student> students = new ArrayList<>();
+        students.add(st1);
+        students.add(st2);
+        students.add(st3);
+        students.add(st4);
+        students.add(st5);
+        StudentInfo studentInfo = new StudentInfo();
+        StudentChecks sc = p -> p.course > 2;
+
+        studentInfo.testStudents(students, student -> student.avgGrade > 8);
+        studentInfo.testStudents(students, sc);
+
+        Collections.sort(students,(student1, student2) -> student1.course - student2.course);
+        System.out.println(students);
+    }
+
+    void testStudents(ArrayList<Student> arrayList, StudentChecks studentChecks) {
+        for (Student student : arrayList) {
+            if (studentChecks.check(student)) {
+                System.out.println(student);
+            }
+        }
+        System.out.println("----------");
+    }
+}
