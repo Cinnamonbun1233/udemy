b308999a06613b6b55c8065cf04247e792c915d5
cinnamonbun1233 <cinnamonbun1233@yandex.ru>
2023-05-10 11:15:02 +0300

    Update

---
+++ Leonid_Tischkevich/src/java_start/part_001/lesson_012/booking/entity/Bill.java
@@ -0,0 +1,17 @@
+package java_start.part_001.lesson_012.booking.entity;
+
+public class Bill {
+    private Integer amount;
+
+    public Bill(Integer amount) {
+        this.amount = amount;
+    }
+
+    public Integer getAmount() {
+        return amount;
+    }
+
+    public void setAmount(Integer amount) {
+        this.amount = amount;
+    }
+}
