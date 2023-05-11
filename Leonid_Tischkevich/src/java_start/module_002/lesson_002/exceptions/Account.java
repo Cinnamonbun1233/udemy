d3000978c9eb13d8cabc82590f9bdd81c5faea68
cinnamonbun1233 <cinnamonbun1233@yandex.ru>
2023-05-11 12:27:08 +0300

    Update

---
+++ Leonid_Tischkevich/src/java_start/module_002/lesson_002/exceptions/Account.java
@@ -0,0 +1,46 @@
+package java_start.module_002.lesson_002.exceptions;
+
+public class Account {
+    private Long id;
+    private String name;
+    private Integer bill;
+
+    public Account(Long id, String name, Integer bill) {
+        this.id = id;
+        this.name = name;
+        this.bill = bill;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getBill() {
+        return bill;
+    }
+
+    public void setBill(Integer bill) {
+        this.bill = bill;
+    }
+
+    @Override
+    public String toString() {
+        return "Account{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", bill=" + bill +
+                '}';
+    }
+}
